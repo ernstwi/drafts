450823483d9b7df980c00bef94b1b0adfc19d497
Ernst Widerberg <ernstwi@kth.se>
2023-01-31 16:52:14 +0100

    feat(cli)!: Remove `query` subcommand

--- cli/main.go
+++ cli/main.go
@@ -23,24 +23,12 @@ type GetCmd struct {
 	UUID string `arg:"positional" help:"UUID (omit to use active draft)"`
 }
 
-type QueryCmd struct {
-	QueryString string   `arg:"positional" help:"search string"`
-	Filter      string   `arg:"-f" help:"filter: inbox, flagged, archive, or trash" default:"all"`
-	Tags        []string `arg:"-t,--tag,separate" help:"filter by tag"`
-	OmitTags    []string `arg:"-T,--omit-tag,separate" help:"filter out by tag"`
-	Sort        string   `arg:"-s" help:"sort method: created, modified, or accessed"`
-	// TODO: Replace with SortAscending? Make descending default.
-	SortDescending   bool `arg:"-d,--descending" help:"sort descending"`
-	SortFlaggedToTop bool `arg:"-F,--flagged-first" help:"sort flagged drafts to top"`
-}
-
 type SelectCmd struct{}
 
 func main() {
 	var args struct {
 		New    *NewCmd    `arg:"subcommand:new" help:"create new draft"`
 		Get    *GetCmd    `arg:"subcommand:get" help:"get content of draft"`
-		Query  *QueryCmd  `arg:"subcommand:query" help:"search for drafts"`
 		Select *SelectCmd `arg:"subcommand:select" help:"select active draft using fzf"`
 	}
 	p := arg.MustParse(&args)
@@ -52,10 +40,6 @@ func main() {
 		fmt.Println(new(p, args.New))
 	case args.Get != nil:
 		fmt.Println(get(p, args.Get.UUID))
-	case args.Query != nil:
-		for _, d := range query(p, args.Query) {
-			fmt.Println(d.String())
-		}
 	case args.Select != nil:
 		_select()
 	}
@@ -91,46 +75,6 @@ func get(p *arg.Parser, uuid string) string {
         return drafts.Get(drafts.Active())
 	}
 	return drafts.Get(uuid)
-}
-
-func query(p *arg.Parser, param *QueryCmd) []drafts.Draft {
-	opt := drafts.QueryOptions{
-		Tags:             param.Tags,
-		OmitTags:         param.OmitTags,
-		SortDescending:   param.SortDescending,
-		SortFlaggedToTop: param.SortFlaggedToTop,
-	}
-
-	// TODO: Custom parsing
-	// https://github.com/alexflint/go-arg#custom-parsing
-
-	var filter drafts.Filter
-	switch param.Filter {
-	case "all":
-		filter = drafts.FilterAll
-	case "inbox":
-		filter = drafts.FilterInbox
-	case "archive":
-		filter = drafts.FilterArchive
-	case "trash":
-		filter = drafts.FilterTrash
-	default:
-		p.Fail("filter must be inbox, flagged, archive, or trash")
-	}
-
-	switch param.Sort {
-	case "":
-	case "created":
-		opt.Sort = drafts.SortCreated
-	case "modified":
-		opt.Sort = drafts.SortModified
-	case "accessed":
-		opt.Sort = drafts.SortAccessed
-	default:
-		p.Fail("sort must be created, modified, or accessed")
-	}
-
-	return drafts.Query(param.QueryString, filter, opt)
 }
 
 func _select() {
