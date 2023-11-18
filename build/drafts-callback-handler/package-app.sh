b87208fb010e76e8ee20d47372580657f9231491
Ernst Widerberg <ernstwi@kth.se>
2023-11-18 14:35:33 +0100

    Fix: Package script is run once per build target

--- build/drafts-callback-handler/package-app.sh
+++ build/drafts-callback-handler/package-app.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Run from root of repo
+# Setup
+mkdir -p "Drafts Callback Handler.app/Contents/MacOS"
+cp "build/drafts-callback-handler/Info.plist" "Drafts Callback Handler.app/Contents/"
 
-function setup() {
-	mkdir -p "Drafts Callback Handler.app/Contents/MacOS"
-	cp "build/drafts-callback-handler/Info.plist" "Drafts Callback Handler.app/Contents/"
-}
-
-rm -f *.zip
-
-setup
-cp "dist/drafts-callback-handler_darwin_amd64_v1/drafts-callback-handler" "Drafts Callback Handler.app/Contents/MacOS/"
-zip -r "drafts-callback-handler-intel.zip" "Drafts Callback Handler.app/"
-rm -rf "Drafts Callback Handler.app"
-
-setup
-cp "dist/drafts-callback-handler_darwin_arm64/drafts-callback-handler" "Drafts Callback Handler.app/Contents/MacOS/"
-zip -r "drafts-callback-handler-arm.zip" "Drafts Callback Handler.app/"
+cp $BIN "Drafts Callback Handler.app/Contents/MacOS/"
+zip -r "drafts-callback-handler-app_$TARGET.zip" "Drafts Callback Handler.app/"
 rm -rf "Drafts Callback Handler.app"
