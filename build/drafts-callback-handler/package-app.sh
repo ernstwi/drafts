964c6c9182d66970991cfa01f0c07e0c0bc75d9e
Ernst Widerberg <ernstwi@kth.se>
2023-11-18 13:51:36 +0100

    Fix script + simplify archive name

--- build/drafts-callback-handler/package-app.sh
+++ build/drafts-callback-handler/package-app.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# cd to script directory
-cd "$(dirname "$0")"
+# Run from root of repo
 
 # Create .app structure
 mkdir -p "Drafts Callback Handler.app/Contents/MacOS"
-cp "../../dist/drafts-callback-handler_darwin_arm64/drafts-callback-handler" "Drafts Callback Handler.app/Contents/MacOS/"
-cp "Info.plist" "Drafts Callback Handler.app/Contents/"
+cp "dist/drafts-callback-handler_darwin_arm64/drafts-callback-handler" "Drafts Callback Handler.app/Contents/MacOS/"
+cp "build/drafts-callback-handler/Info.plist" "Drafts Callback Handler.app/Contents/"
 
 # Zip it
-zip -r "Drafts Callback Handler.zip" "Drafts Callback Handler.app/"
+zip -r "drafts-callback-handler.zip" "Drafts Callback Handler.app/"
 rm -r "Drafts Callback Handler.app"
