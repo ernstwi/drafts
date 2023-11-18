973c08e71025e874dcbab9ca97d57e39a14dd376
Ernst Widerberg <ernstwi@kth.se>
2023-11-18 10:47:13 +0100

    Complete app package

--- build/drafts-callback-handler/package-app.sh
+++ build/drafts-callback-handler/package-app.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# cd to script directory
+cd "$(dirname "$0")"
+
 # Create .app structure
-mkdir -p "DraftsCallbackHandler.app/Contents/MacOS"
-cp "../../dist/drafts-callback-handler_darwin_arm64/drafts-callback-handler" "DraftsCallbackHandler.app/Contents/MacOS/"
-cp "Info.plist" "DraftsCallbackHandler.app/Contents/"
+mkdir -p "Drafts Callback Handler.app/Contents/MacOS"
+cp "../../dist/drafts-callback-handler_darwin_arm64/drafts-callback-handler" "Drafts Callback Handler.app/Contents/MacOS/"
+cp "Info.plist" "Drafts Callback Handler.app/Contents/"
 
 # Zip it
-zip -r "DraftsCallbackHandler.zip" "DraftsCallbackHandler.app/"
+zip -r "Drafts Callback Handler.zip" "Drafts Callback Handler.app/"
+rm -r "Drafts Callback Handler.app"
