0b7f8ff352d07364342cbc661ec7a084696dc6ab
Ernst Widerberg <ernstwi@kth.se>
2023-11-25 08:53:30 +0100

    Don't zip app

--- build/drafts-callback-handler/package-app.sh
+++ build/drafts-callback-handler/package-app.sh
@@ -5,5 +5,3 @@ mkdir -p "Drafts Callback Handler.app/Contents/MacOS"
 cp "build/drafts-callback-handler/Info.plist" "Drafts Callback Handler.app/Contents/"
 
 cp $BIN "Drafts Callback Handler.app/Contents/MacOS/"
-zip -r "drafts-callback-handler-app_$TARGET.zip" "Drafts Callback Handler.app/"
-rm -rf "Drafts Callback Handler.app"
